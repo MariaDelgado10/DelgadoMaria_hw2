3741c003b94235f70165575c0b7dd84de04781cc
MariaDelgado10 <49251994+MariaDelgado10@users.noreply.github.com>
2019-05-08 10:37:07 -0500

    Cambios en tabs de makefile, aun no funciona bien

--- hw2.mk
+++ hw2.mk
@@ -1,22 +1,22 @@
-Resultados_hw2.pdf : Resultados_hw2.tex Señales.png Fourier_trans.png  espectograma2señales.png temblor.png transfouriertemblor.png espectogramatemblor.png
-    pdflatex Resultados_hw2.tex
+Resultados_hw2.pdf : Resultados_hw2.tex Señales.pdf Fourier_trans.pdf espectograma2señales.pdf temblor.pdf transfouriertemblor.pdf espectogramatemblor.pdf
+	pdflatex Resultados_hw2.tex
 
-Señales.png : Fourier.py signal.dat signalSuma.dat
-    python Fourier.py
+Señales.pdf : Fourier.py signal.dat signalSuma.dat
+	python Fourier.py
 
-Fourier_trans.png : Fourier.py signal.dat signalSuma.dat
-    python Fourier.py
+Fourier_trans.pdf : Fourier.py signal.dat signalSuma.dat
+	python Fourier.py
     
-espectograma2señales.png : Fourier.py signal.dat signalSuma.dat
-    python Fourier.py   
+espectograma2señales.pdf : Fourier.py signal.dat signalSuma.dat
+	python Fourier.py   
     
 temblor.png : Fourier.py temblor.txt
-    python Fourier.py  
+	python Fourier.py  
     
-transfouriertemblor.png : Fourier.py temblor.txt
-    python Fourier.py 
+transfouriertemblor.pdf : Fourier.py temblor.txt
+	python Fourier.py 
 
-espectogramatemblor.png : Fourier.py temblor.txt
-    python Fourier.py 
+espectogramatemblor.pdf : Fourier.py temblor.txt
+	python Fourier.py 
 
     
